443870b494f362904fc1b5b887c5aaadcce2ab85
Santhoshg059 <g.santhosh059@gmail.com>
2024-03-17 13:25:07 +0530

    add vercel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -99,7 +99,7 @@ function Home() {
             </div>
             {/* Import JavaScript files using script tags */}
             {/* <script src="script.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+            <src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script> */}
              <MDBFooter className='bg-light text-center text-white'>
       <MDBContainer className='p-4 pb-0'>
